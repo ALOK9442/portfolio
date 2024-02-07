816400ad6f7b5e87e93610f8659c30be791a6bc4
ALOK9442 <alok2110036@akgec.ac.in>
2024-02-07 18:06:04 +0530

    added dark mode

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,12 +63,12 @@ const Navbar = () => {
         {/* <motion.a href="/" target='_blank' whileHover={{y:-2}} className='w-6 mx-3 bg-light rounded-full'><PinterestIcon/></motion.a> */}
         {/* <motion.a href="/" target='_blank' whileHover={{y:-2}} className='w-6 ml-3'><DribbbleIcon/></motion.a> */}
 
-        {/* <button onClick={()=>{setmode(mode === "light" ? "dark" : "light")}} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode =="light" ? "bg-dark text-light":"bg-light text-dark"}`}>
+        <button onClick={()=>{setmode(mode === "light" ? "dark" : "light")}} className={`ml-3 flex items-center justify-center rounded-full p-1 ${mode =="light" ? "bg-dark text-light":"bg-light text-dark"}`}>
  
  {
    mode ==="dark" ? <SunIcon className={"fill-dark"} /> :<MoonIcon className={"fill-dark"}/>
  }
-</button> */}
+</button>
         {/* </nav> */}
       </div>
 
